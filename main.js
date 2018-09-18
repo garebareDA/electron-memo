e909719d9030d6a508029e67e3815a222a60aaf7
gann <daiki_17n1100521@nnn.ed.jp>
2018-09-18 12:59:14 +0900

    first commit

--- main.js
+++ main.js
@@ -38,7 +38,6 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
